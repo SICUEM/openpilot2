e08d7035ba2ba9e5b802434d0c66d9bfb52ac4b4
Javier Fernandez <javier.fernandez@universidadeuropea.es>
2023-11-16 16:08:36 +0100

    Update __init__.py

--- kstopmanv/__init__.py
+++ kstopmanv/__init__.py
@@ -10,9 +10,9 @@ RESUME_D = 60       # Longitud estado RESUME (m)
 
 IN_AREA_V = 20      # Velocidad de estado IN_AREA (k/h)
 APPROACHING_V = 10  # Velocidad de estado APPROACHING (k/h)
-RESUME_V = 50       # Velocidad que se ha de 
+RESUME_V = 30       # Velocidad que se ha de 
                     # alcanzar en estado RESUME
-RESUME_A = 1      # Aceleración aplicada en el 
+RESUME_A = 0.5      # Aceleración aplicada en el 
                     # estado RESUME (m) (no se usa)
             
 STOP_V_THRES = 1    # Umbral de velocidad a 0 (k/h)
