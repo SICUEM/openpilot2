9aede9a885174f5979072b4a0b1d731bd1f451fd
Jorge García <99172408+jorgeRT524@users.noreply.github.com>
2024-03-09 20:19:19 +0100

    Update controlsd.py
    Pasado CS a texto por defecto para prueba de envío de datos

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -466,7 +466,8 @@ class Controls:
       info_tlmtry_q = queue.Queue()
     
     # Colocar mensaje en la cola antes de iniciar el productor
-      info_tlmtry_q.put({"velocidad": str(CS.vEgo) + " km/h"})
+    # info_tlmtry_q.put({"velocidad": str(CS.vEgo) + " km/h"})
+      info_tlmtry_q.put({"velocidad": "20 km/h"})  
       info_tlmtry_q.put({"accelerador": "0.4"})
       info_tlmtry_q.put({"freno": "0.0"})
       info_tlmtry_q.put({"proximo destino": "a 10km"})
